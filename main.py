d75a1b730892090443ce48488f39cc386c6f34a8
multika <github@basepointer.de>
2019-02-15 17:52:51 +0100

    Improved setuid detection and bug fixes

--- main.py
+++ main.py
@@ -1,46 +1,47 @@
 import subprocess
 import os
 import re
+import stat
 
-print('SO Checker')
+print('----- SO Checker -----')
 print('Checking for vulnerable binaries')
+print()
 
 setuid_binaries = {}
-# implement logic to find setuid binaries and binaries started by root as cronjob
-bin_paths = ['/bin', '/sbin', '/usr/bin']
+vulnerable = False
+bin_paths = ['/home/', '/bin', '/sbin', '/usr/bin', '/opt', '/root']
 
+# Search for setuid binaries
 for bin_path in bin_paths:
     for root,dirs,files in os.walk(bin_path):
         for binary in files:
             try:
-                permissions = os.stat(os.path.join(root, binary))
-                if oct(permissions[0])[3] == '4':
+                if os.stat(os.path.join(root, binary)).st_mode & stat.S_ISUID == 2048:
                     setuid_binaries[os.path.join(root, binary)] = 0
             except:
                 pass
 
-print(setuid_binaries)
-
 for binary in setuid_binaries:
-    # Check if binary uses shared library
+    # Check if binary uses shared libraries
     setuid_binaries[binary] = ['/lib', '/usr/lib']
     shared_libs = []
     output_raw = subprocess.Popen(['ldd', binary], stdout=subprocess.PIPE).communicate()
     regex = r"[^\\t ]/?[a-z0-9-_]*/?[a-z0-9-_]*/?[a-z0-9-_]+\.[a-z0-9-_]+[a-zA-Z0-9-_\.]+"
     matches = re.finditer(regex, str(output_raw))
+
     for match in matches:
         shared_libs.append(match.group())
-    print('{} shared libraries found.'.format(len(shared_libs))) 
 
     if shared_libs:
         # Check for RPATH and DT_RUNPATH linker options
         output_raw = subprocess.Popen(['objdump', '-x', binary], stdout=subprocess.PIPE).communicate()
-        for item in output_raw[0].split('\n'):
+        for item in output_raw[0].decode().split('\n'):
+
             if 'RPATH' in item:
                 setuid_binaries[binary].append(item.strip().split()[1])
             if 'RUNPATH' in item:
                 setuid_binaries[binary].append(item.strip().split()[1])
-        
+
         # Check for environment variables
         if 'LD_RUN_PATH' in os.environ:
             setuid_binaries[binary].append(os.environ.get('LD_RUN_PATH'))
@@ -50,10 +51,13 @@ for binary in setuid_binaries:
         # Check ld.so.conf
         # TBD
 
-# print(lib_paths)
 for binary in setuid_binaries:
     for lib_path in setuid_binaries[binary]:
+
         if os.access(lib_path, os.W_OK):
-            print(binary + ' is vulnerable:')
-            print(lib_path + ' is writable!')
-            print
+            print('{} is vulnerable:'.format(binary))
+            print('{} is writable'.format(lib_path))
+            vulnerable = True
+
+if vulnerable == False:
+    print('No vulnerable binaries found.')
